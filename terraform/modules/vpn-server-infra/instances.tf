6e06721cd3665dfdfb4ab63e5b402e524992b8e3
onukwilip <onukwilip@gmail.com>
2026-08-24 22:04:59 +0100

    Separated firewall resource for netbird server access from Instance config

--- terraform/modules/vpn-server-infra/instances.tf
+++ terraform/modules/vpn-server-infra/instances.tf
@@ -63,25 +63,3 @@ resource "google_compute_instance" "netbird_server" {
     google_service_account.netbird_server
   ]
 }
-
-resource "google_compute_firewall" "allow_netbird_server_access" {
-  name    = "allow-netbird-server-access"
-  project = var.project_id
-  network = var.network
-
-  direction = "INGRESS"
-  priority  = 1000
-
-  source_ranges = ["0.0.0.0/0"]
-
-  target_tags = [var.netbird_server_network_tag]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-}
